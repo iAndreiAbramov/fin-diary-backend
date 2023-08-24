80ab8cdbaafa4bb218b40259c7a92333fbed5d4e
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-08-24 20:50:06 +0300

    PrismaModule created

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from '@src/module/app-config/app-config.module';
 import { AuthModule } from '@src/module/auth/auth.module';
+import { PrismaModule } from '@src/module/prisma/prisma.module';
 
 @Module({
-  imports: [AppConfigModule, AuthModule],
+  imports: [AppConfigModule, AuthModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from '@src/module/auth/auth.controller';
 import { AuthService } from '@src/module/auth/auth.service';
+import { PrismaModule } from '@src/module/prisma/prisma.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [PrismaModule],
 })
 export class AuthModule {
 }
