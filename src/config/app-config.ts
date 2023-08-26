ca2e37b745d4e96c32a02aa183b298faaebed449
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-08-26 20:30:25 +0300

    feat: add app config

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,16 +2,19 @@ import { registerAs } from '@nestjs/config';
 import * as Joi from 'joi';
 import { IAppConfig } from '@src/types/app-config.interface';
 import { AppMode } from '@src/types/app-mode.enum';
+import * as process from 'process';
 
 export default registerAs('app', (): IAppConfig => {
   const config: IAppConfig = {
     port: parseInt(process.env.APP_PORT),
     mode: process.env.APP_MODE as AppMode,
+    saltRounds: parseInt(process.env.SALT_ROUNDS),
   };
 
   const schema = Joi.object<IAppConfig, false, IAppConfig>({
     port: Joi.number().port().required(),
-    mode: Joi.string().valid(AppMode.Dev, AppMode.Prod),
+    mode: Joi.string().valid(AppMode.Dev, AppMode.Prod).default(AppMode.Dev),
+    saltRounds: Joi.number().required(),
   });
 
   const { error } = schema.validate(config);
--- src/types/app-config.interface.ts
+++ src/types/app-config.interface.ts
@@ -3,4 +3,5 @@ import { AppMode } from './app-mode.enum';
 export interface IAppConfig {
   port: number | undefined;
   mode: AppMode | undefined;
+  saltRounds: number;
 }
