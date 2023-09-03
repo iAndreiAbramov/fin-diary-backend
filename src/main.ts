db13a9dae816a0f2fe6a83ad899b2ba4287f8ad7
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-09-03 17:54:24 +0300

    feat: @nestjs/swagger installed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap(): Promise<void> {
   const appConfig = app.get(ConfigService);
   const port = appConfig.getOrThrow('app.port');
   const mode = appConfig.getOrThrow('app.mode');
+
+  const config = new DocumentBuilder()
+    .setTitle('Fin diary API')
+    .setVersion('1.0')
+    .addTag('Fin diary')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 
