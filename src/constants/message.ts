86f6b45971b8275b1952d29192e9180b56819165
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-10-15 10:20:25 +0300

    refactor: error messages translated to russian

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,6 +1,6 @@
 export const Message = {
-  UserWithEmailExists: (email: string): string => `User with email: ${email} already exists`,
-  UserWithEmailNotFound: (email: string): string => `User with email: ${email} not found`,
-  UserWithIdNotFound: (id: number): string => `User with id: ${id} not found`,
-  PasswordIsIncorrect: () => 'Email or password is incorrect',
+  UserWithEmailExists: (email: string): string => `Пользователь с email: ${email} уже существует`,
+  UserWithEmailNotFound: (email: string): string => `Пользователь с email: ${email} не найден`,
+  UserWithIdNotFound: (id: number): string => `Польщователь с id: ${id} не найден`,
+  PasswordIsIncorrect: () => 'Email или пароль указаны неверно',
 } as const;
