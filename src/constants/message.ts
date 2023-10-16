628197d12bb1666534fff34f372167c66ff5a2af
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-10-16 21:56:37 +0300

    refactor: updates change-password dto

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,6 +1,6 @@
 export const Message = {
   UserWithEmailExists: (email: string): string => `Пользователь с email: ${email} уже существует`,
   UserWithEmailNotFound: (email: string): string => `Пользователь с email: ${email} не найден`,
-  UserWithIdNotFound: (id: number): string => `Польщователь с id: ${id} не найден`,
+  UserWithIdNotFound: (id: number): string => `Пользователь с id: ${id} не найден`,
   PasswordIsIncorrect: () => 'Email или пароль указаны неверно',
 } as const;
